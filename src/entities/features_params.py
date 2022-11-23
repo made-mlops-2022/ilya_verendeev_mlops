8cc6fcb63686855d16c85a77ae5760d850628bb1
ilya <ilaverendeev@gmail.com>
2022-11-23 14:45:14 +0300

    first commit

--- src/entities/features_params.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-from dataclasses import dataclass
-
-
-@dataclass()
-class FeatureParams:
-    categorical_features: List[str]
-    numerical_features: List[str]
-    target_col: str
